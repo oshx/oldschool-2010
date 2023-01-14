be7725a8932e04c0b1e7b26416043f5d27833e46
오승환 <oshxios@gmail.com>
2023-01-14 17:45:39 +0900

    Changed config for private preferences

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import type { Router, RouteRecordRaw } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue')
-    }
-  ]
-})
+const history = createWebHistory(import.meta.env.BASE_URL);
 
-export default router
+export const homeRoute: RouteRecordRaw = {
+  name: 'home',
+  path: '/',
+  component: HomeView,
+};
+
+export const aboutRoute: RouteRecordRaw = {
+  name: 'about',
+  path: '/about',
+  component: () => import('@/views/AboutView.vue'),
+};
+
+export const routes: RouteRecordRaw[] = [homeRoute, aboutRoute];
+
+const router: Router = createRouter({
+  history,
+  routes,
+});
+
+export default router;
